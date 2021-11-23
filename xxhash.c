4059a284f1ce28daf0d74a67b115d5288f6ec337
Dylan K. Taylor <dktapps@pmmp.io>
2021-11-23 20:45:44 +0000

    xxhash has APIs for byte order normalization

--- xxhash.c
+++ xxhash.c
@@ -13,40 +13,30 @@
 
 PHP_FUNCTION(xxhash32) {
 	zend_string *input;
-	unsigned char result[4];
 
 	ZEND_PARSE_PARAMETERS_START_EX(ZEND_PARSE_PARAMS_THROW, 1, 1)
 		Z_PARAM_STR(input)
 	ZEND_PARSE_PARAMETERS_END();
 
 	XXH32_hash_t hash = XXH32(ZSTR_VAL(input), ZSTR_LEN(input), 0);
-	result[0] = (unsigned char) ((hash >> 24) & 0xff);
-	result[1] = (unsigned char) ((hash >> 16) & 0xff);
-	result[2] = (unsigned char) ((hash >> 8) & 0xff);
-	result[3] = (unsigned char) (hash & 0xff);
+	XXH32_canonical_t canonicalHash;
+	XXH32_canonicalFromHash(&canonicalHash, hash);
 
-	RETURN_STRINGL((const char *) &result, sizeof(result));
+	RETURN_STRINGL((const char *) &canonicalHash.digest, sizeof(canonicalHash.digest));
 }
 
 PHP_FUNCTION(xxhash64) {
 	zend_string *input;
-	unsigned char result[8];
 
 	ZEND_PARSE_PARAMETERS_START_EX(ZEND_PARSE_PARAMS_THROW, 1, 1)
 		Z_PARAM_STR(input)
 	ZEND_PARSE_PARAMETERS_END();
 
 	XXH64_hash_t hash = XXH64(ZSTR_VAL(input), ZSTR_LEN(input), 0);
-	result[0] = (unsigned char) ((hash >> 56) & 0xff);
-	result[1] = (unsigned char) ((hash >> 48) & 0xff);
-	result[2] = (unsigned char) ((hash >> 40) & 0xff);
-	result[3] = (unsigned char) ((hash >> 32) & 0xff);
-	result[4] = (unsigned char) ((hash >> 24) & 0xff);
-	result[5] = (unsigned char) ((hash >> 16) & 0xff);
-	result[6] = (unsigned char) ((hash >> 8) & 0xff);
-	result[7] = (unsigned char) (hash & 0xff);
+	XXH64_canonical_t canonicalHash;
+	XXH64_canonicalFromHash(&canonicalHash, hash);
 
-	RETURN_STRINGL((const char *) &result, sizeof(result));
+	RETURN_STRINGL((const char *) &canonicalHash.digest, sizeof(canonicalHash.digest));
 }
 
 PHP_MINFO_FUNCTION(xxhash)
